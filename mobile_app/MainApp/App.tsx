3e29ae1c1fd99eef3912627e5b19006399dfa776
eszayna <eszayna@gmail.com>
2024-04-10 01:21:41 -0700

    app - make a src folder for components and screens

--- mobile_app/MainApp/App.tsx
+++ mobile_app/MainApp/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 // Page imports
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
   return (
