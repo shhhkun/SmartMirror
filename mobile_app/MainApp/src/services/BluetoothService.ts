04abcf963c2dcd929b903c2996396a07e86c0f1f
eszayna <eszayna@gmail.com>
2024-04-10 20:12:58 -0700

    app - put in starter class for bluetooth service

--- mobile_app/MainApp/src/services/BluetoothService.ts
+++ mobile_app/MainApp/src/services/BluetoothService.ts
@@ -1,1 +1,31 @@
-// todo
+// wrapper class for react-native-ble-manager
+
+import BleManager from 'react-native-ble-manager';
+
+class BluetoothService {
+  static initialize(): void {
+    BleManager.start();
+  }
+
+  static scan(): Promise<any> {
+    return BleManager.scan([], 5, true);
+  }
+
+  static connect(deviceUUID: string): Promise<void> {
+    return BleManager.connect(deviceUUID);
+  }
+
+  static read(deviceUUID: string, serviceUUID: string, characteristicUUID: string): Promise<any> {
+    return BleManager.read(deviceUUID, serviceUUID, characteristicUUID);
+  }
+
+  static write(deviceUUID: string, serviceUUID: string, characteristicUUID: string, data: number[]): Promise<void> {
+    return BleManager.write(deviceUUID, serviceUUID, characteristicUUID, data);
+  }
+
+  static disconnect(deviceUUID: string): Promise<void> {
+    return BleManager.disconnect(deviceUUID);
+  }
+}
+
+export default BluetoothService;
