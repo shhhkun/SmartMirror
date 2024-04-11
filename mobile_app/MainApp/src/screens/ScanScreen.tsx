857752ea6d1a47b4468cfdf3adec262f434c47ff
eszayna <eszayna@gmail.com>
2024-04-10 22:30:10 -0700

    app - put some text on the scan screen page

--- mobile_app/MainApp/src/screens/ScanScreen.tsx
+++ mobile_app/MainApp/src/screens/ScanScreen.tsx
@@ -1,1 +1,44 @@
-// todo
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { GlobalStyles } from '../common/GlobalStyles';
+
+import ButtonToNavigate from '../components/ButtonToNavigate';
+import NiceTextArea from '../components/NiceTextArea';
+
+
+const ScanScreen = () => {
+  return (
+    <View style={styles.mainStyle}>
+      <StatusBar></StatusBar>
+
+      <View style={styles.mainStyle}>
+        <NiceTextArea title="Scan for Devices">
+          test
+        </NiceTextArea>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <ButtonToNavigate onPress={console.log("hi")} title="Action Button" />
+      </View>
+
+    </View >
+  );
+};
+
+const styles = StyleSheet.create({
+  mainStyle: {
+    backgroundColor: GlobalStyles.lightBackground,
+  },
+
+  buttonContainer: {
+    // flex: 1, // using flexbox here is cursed; don't do
+    paddingTop: 15,
+    paddingBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: GlobalStyles.lightBackground,
+  },
+
+});
+
+export default ScanScreen;
