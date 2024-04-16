cf2e437f5c882adcb2fed32606c2a0ec05d7a01c
eszayna <eszayna@gmail.com>
2024-04-16 02:13:57 -0700

    app - had a little bug in scan's method signature

--- mobile_app/MainApp/src/services/BluetoothService.ts
+++ mobile_app/MainApp/src/services/BluetoothService.ts
@@ -6,21 +6,21 @@ class BluetoothService {
   static async initialize(): Promise<void> {
     try {
       await BleManager.start({ showAlert: true });
-      console.log("BLE manager initialized");
+      console.log("(BluetoothService.initialize) BLE manager initialized");
 
     } catch (error) {
-      console.error('Error initializing BLE manager:', error);
+      console.error('(BluetoothService.initialize) Error initializing BLE manager:', error);
       throw error; // Re-throw the error to propagate it to the caller if needed
     }
   }
 
-  static async scan(serviceUUIDs: string[], seconds: number, allowDuplicates: boolean): Promise<void> {
+  static async scan(): Promise<void> {
     try {
-      await BleManager.scan(serviceUUIDs, seconds, allowDuplicates);
-      console.log("Scan started");
+      await BleManager.scan([], 5, true);
+      console.log("(BluetoothService.initialize) Scan started");
 
     } catch (error) {
-      console.error('Error starting scan:', error);
+      console.error('(BluetoothService.initialize) Error starting scan:', error);
       throw error; // Re-throw the error to propagate it to the caller if needed
     }
   }
