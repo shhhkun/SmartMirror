a453502c639cbe098810ca90ba21fbfac490655e
alitokii <13807422+alitokii@users.noreply.github.com>
2024-06-06 13:34:40 -0700

    j

--- App/js/userId.js
+++ App/js/userId.js
@@ -1,3 +1,13 @@
 exports.userId = "user 1";
-exports.existingUsers = ["defaultStart","default","Daniel","Serjo","Erick","user 5","user 4","user 1"];
+exports.existingUsers = [
+  "defaultStart",
+  "default",
+  "Daniel",
+  "Serjo",
+  "Erick",
+  "user 5",
+  "user 4",
+  "user 1"
+];
 exports.numUsers = 8;
+// random text
